60a66d1da2bab1158e7cfa7f37455bf4e7eec2d8
Hugo Boudalier <hugo.boudalier@edu.devinci.fr>
2021-06-03 12:10:11 +0200

    add page 404

--- wp-content/themes/rockyourpodcast/404.php
+++ wp-content/themes/rockyourpodcast/404.php
@@ -13,43 +13,18 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'rockyourpodcast' ); ?></h1>
-			</header><!-- .page-header -->
+		<div class="page-content">
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'rockyourpodcast' ); ?></p>
+				<span>404</span>
+				<h2 class="page-title"><?php esc_html_e( "La page que vous cherchez semble introuvable... ", 'rockyourpodcast' ); ?></h2>
 
-					<?php
-					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+		
+			<div class = "buttonHome">
+				<a href="accueil">Retourner sur l'accueil</a>
+			</div>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'rockyourpodcast' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$rockyourpodcast_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'rockyourpodcast' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$rockyourpodcast_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
